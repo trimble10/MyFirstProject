8043e222325ba6ab26066c7458d521e0e719075c
trimble10 <70170121+trimble10@users.noreply.github.com>
2020-08-25 15:02:57 -0500

    Add project files.

---
+++ MyFirstProject/Main.cpp
@@ -0,0 +1,14 @@
+/*
+	Aiden Trimble
+	C++ 2020
+	Due: August 31st
+	Lab 1 Introduction to C++ Programming
+	The goals are for you to download a project, compile and run that project, and to upload your own project.
+
+*/
+#include <iostream>
+int main()
+{
+	std::cout << "HelloWorld! Red";
+	return 0;
+}
